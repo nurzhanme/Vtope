e7d47ea5ed8cd90bbd8b0100e8c3854101801169
Nurzhan Aitbayev <nurzhan@outlook.com>
2022-08-09 04:51:37 +0600

    update SessionData nullable

--- Vtope/Models/InstaAccount.cs
+++ Vtope/Models/InstaAccount.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
-        public string SessionData { get; set; }
+        public string? SessionData { get; set; }
         public bool IsUtil { get; set; }
     }
 }
